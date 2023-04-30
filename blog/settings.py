37a8a914df49e43063bd0caf858613f9bef7c659
Tawheed <tawheed.chilwan55@gmail.com>
2023-04-30 18:44:10 +0530

    final settings

--- blog/settings.py
+++ blog/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from dotenv import load_dotenv
+load_dotenv()
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5+(i-^!lq7im8l-$0srkl=^6t96@lf(ix*5!do13ke(@h32d_l'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
